0b6dae55a7ccb07e970b687f7158728b49cbebc4
Meos <meos.pro@gmail.com>
2024-01-09 20:25:52 +0100

    Change tooltip duration to infinite

--- src/FrontendQt/TweaksWidget.cpp
+++ src/FrontendQt/TweaksWidget.cpp
@@ -1,6 +1,7 @@
 #include "TweaksWidget.hpp"
 
 #include "Common/DstException.hpp"
+#include "FrontendQt/GuiSettings.hpp"
 
 #include "nlohmann/json.hpp"
 
@@ -31,53 +32,55 @@ TweaksWidget::TweaksWidget(QWidget* parent)
 		{ SETTINGS(m_ui.duckStation60FPS) }
 	};
 
-	m_ui.tweaksUnlockCostumeByDefault->setToolTip("Allows you to choose secondary costumes even if you have never beaten the game.");
-	m_ui.tweaksExpandHeroAndLegendShops->setToolTip
-	(
+	GuiSettings::setToolTip(m_ui.tweaksUnlockCostumeByDefault, 
+		"Allows you to choose secondary costumes even if you have never beaten the game.");
+
+	GuiSettings::setToolTip(m_ui.tweaksExpandHeroAndLegendShops,
 		"For some reasons the Hero and Legend shops use the Novice shop, this will\n"
 		"expand them according to the growth of the Master shop but with better bonuses."
 	);
-	m_ui.tweaks60To50LegendDifficultyLevelRequirement->setToolTip
-	("Lower the Legend's difficulty requirements to 50 to make it more accessible.");
-	m_ui.tweaksPermanentShopsItems->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.tweaks60To50LegendDifficultyLevelRequirement,
+		"Lower the Legend's difficulty requirements to 50 to make it more accessible.");
+
+	GuiSettings::setToolTip(m_ui.tweaksPermanentShopsItems,
 		"Some items like spell books, jewels or elixirs can only be purchased once from the shop and require\n"
 		"closing and then reopening the shop to bring them back, using this feature will make all items permanent."
 	);
-	m_ui.tweaksSpellDurability3Stacks->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.tweaksSpellDurability3Stacks,
 		"Can stack 3 times the durability of spell buffs.\n\n"
 		"Ex: A level 1 Berserker that has 33 seconds can be cast 3 times to get 99 seconds."
 	);
-	m_ui.tweaksTalkToNPCsWhileInvisible->setToolTip("Allows you to talk to NPCs when Invisibility is active.");
-	m_ui.tweaksHideLevelUpXPIfStatsAreAtMaximum->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.tweaksTalkToNPCsWhileInvisible, "Allows you to talk to NPCs when Invisibility is active.");
+	GuiSettings::setToolTip(m_ui.tweaksHideLevelUpXPIfStatsAreAtMaximum,
 		"Due to elixirs you can reach the limit of your stats, they will prevent you from distributing your\n"
 		"points but the level up \"XP!\" will always be displayed in the hud which can be annoying, this feature hides it."
 	);
-	m_ui.fixesExpBarGlitch->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.fixesExpBarGlitch,
 		"From level 50 due to a type overflow the exp bar is not displayed correctly, this fixes it."
 	);
-	m_ui.fixesCantSaveIfLevel59->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.fixesCantSaveIfLevel59,
 		"When the game saves your character it checks how much exp you have, if you have\n"
 		"less it doesn't save. The problem is that the amount of exp is the same from level 59.\n\n"
 		"Ex: If your character is level 59 with 50% and you save level 60 with 0% it will deny it.\n\n"
 		"The fixes will also check the level to solves it."
 	);
-	m_ui.fixesTheftBlock->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.fixesTheftBlock,
 		"Due to an oversight, a Theft variable is not reset when entering a new\n"
 		"map and may block the spell until you open a menu, using this fixes it."
 	);
-	m_ui.fixesTheftEmptyJewelry->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.fixesTheftEmptyJewelry,
 		"Theft will always generate empty jewels as they are on a item pool\n"
 		"that doesn't generate bonuses, this fixes will generate bonuses for them."
 	);
-	m_ui.duckStation60FPS->setToolTip
-	(
+
+	GuiSettings::setToolTip(m_ui.duckStation60FPS,
 		"Turn the game framerate to 60 FPS.\n\n"
 		"DuckStation requirements: Settings -> Console Settings -> CPU Emulation set \"Enable Clock Speed Control\" to 920% for NTSC-U and 750% for PAL"
 	);
